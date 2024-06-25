077e40346b5e7dded821a402c4ad627006e4a900
harsh <hb755037@gmail.com>
2024-06-25 18:57:11 +0530

    last

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, matchPath, useLocation } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai";
+import { BsChevronDown } from "react-icons/bs";
 import logo from "../../assets/Logo/Logo-Full-Light.png";
 import { NavbarLinks } from "../../data/navbar-links";
 import { useSelector } from "react-redux";
 import { ACCOUNT_TYPE } from "../../utils/constants";
 import ProfileDropdown from "../core/Auth/ProfileDropdown";
+import { apiConnector } from "../../services/apiConnector";
+import { categories } from "../../services/apis";
 
 
 const Navbar = () => {
@@ -13,10 +16,46 @@ const Navbar = () => {
   const { user } = useSelector((state) => state.profile);
   const { totalItems } = useSelector((state) => state.cart);
 
+  const [loading, setLoading] = useState(false)
+
   const location = useLocation();
   const matchRoute = (route) => {
     return matchPath({ path: route }, location.pathname);
   };
+
+  const subLinks = [
+    {
+      title: "Python",
+      url: "/catalog/python"
+    },
+    {
+      title: "Python",
+      url: "/catalog/python"
+    }
+  ]
+
+  // api call
+  // const [subLinks, setSubLinks ] = useState([]);
+
+  // const fetchSubLinks = async() => {
+  //   try {
+  //       const result = await apiConnector('GET', categories.CATEGORIES_API );
+  //       console.log("Result of category list: ", result)
+  //       setSubLinks(result.data.data)
+  //   } catch (error) {
+  //     console.log(`Could not fetch the category list : ${error}`)
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   fetchSubLinks();
+  // }, [])
+
+
+
+
+
+
 
   return (
     <div className=" flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700">
@@ -32,7 +71,42 @@ const Navbar = () => {
             {NavbarLinks.map((url, index) => (
               <li key={index}>
                 {url.title === "Catalog" ? (
-                  <div></div>
+                <>
+                <div
+                  className={`group relative flex cursor-pointer items-center gap-1 ${
+                    matchRoute("/catalog/:catalogName")
+                      ? "text-yellow-25"
+                      : "text-richblack-25"
+                  }`}
+                > 
+                  <p>{url.title}</p>
+                  <BsChevronDown />
+                  <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                        {loading ? (
+                          <p className="text-center">Loading...</p>
+                        ) : subLinks.length ? (
+                          <>
+                            {subLinks
+                              ?.filter(
+                                (subLink) => subLink?.courses?.length > 0
+                              )
+                              ?.map((subLink, i) => (
+                                <Link
+                                  to={subLink.url}
+                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                  key={i}
+                                >
+                                  <p>{subLink.title}</p>
+                                </Link>
+                              ))}
+                          </>
+                        ) : (
+                          <p className="text-center">No Courses Found</p>
+                        )}
+                      </div>
+                    </div>
+                  </>
                 ) : (
                   <Link to={url?.path}>
                     <p
---
+++ src/services/apis.js
@@ -0,0 +1,14 @@
+
+const BASE_URL = "http://localhost:5173/api/v1"
+
+// authentication apis
+
+
+
+
+
+
+// categoreis apis
+export const categories = {
+    CATEGORIES_API: BASE_URL + "/course/showAllCategories",
+}
